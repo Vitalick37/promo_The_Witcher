27ca6c81d1b6dfb8c405e332c4bc93a27e96f5da
Vitalik <vit198837@mail.ru>
2020-02-06 14:55:12 +0300

    3_lesson

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
+let mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
     navigation: {
         nextEl: '.arrow',
     },
     loop: true,
+    breakpoints: {
+        540: {
+            slidesPerView: 2, 
+        }
+    },
+});
+
+let menuButton = document.querySelector('.menu-button'),
+    menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('menu-button-active')
+    menu.classList.toggle('header-active');
 });
